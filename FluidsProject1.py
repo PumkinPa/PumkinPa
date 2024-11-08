f4062b879326bc1b21b70eaf974953132d473b35
PumkinPa <98838300+PumkinPa@users.noreply.github.com>
2024-11-08 10:07:54 -0600

    Update FluidsProject1.py

--- FluidsProject1.py
+++ FluidsProject1.py
@@ -69,7 +69,7 @@ def calculate_pressure_drop(velocity, diameter, length, density, viscosity):
     
     
     # Calculate pressure drop
-    dp = (((blasius_equation(Re)) * density * velocity ** 2) / diameter) 
+    dp = (((blasius_equation(Re)) * density * velocity ** 2) / diameter) * 50
     
     return dp
 
